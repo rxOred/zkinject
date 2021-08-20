d73725bcfe1aa5101a6396487e5a1085ebf09984
rxOred <rxored@gmail.com>
2021-08-20 23:13:17 +0000

    types

--- src/zktypes.hh
+++ src/zktypes.hh
@@ -8,11 +8,28 @@ typedef uint64_t u64;
 typedef uint32_t u32;
 typedef uint8_t u8;
 
-typedef Elf64_Ehdr ehdr64;
-typedef Elf64_Phdr phdr64;
-typedef Elf64_Shdr shdr64;
-typedef Elf64_Dyn dynamic64;
-typedef Elf64_Nhdr note64;
-typedef Elf64_Rela rela64;
+#if defined(__x86_64__)
+
+    typedef Elf64_Ehdr  Ehdr;
+    typedef Elf64_Phdr  Phdr;
+    typedef Elf64_Shdr  Shdr;
+    typedef Elf64_Dyn   Dynamic;
+    typedef Elf64_Nhdr  Note;
+    typedef Elf64_Rela  Rela;
+
+    typedef Elf64_Addr  Addr;
+
+#elif(__i386__)
+
+    typedef Elf32_Ehdr  Ehdr;
+    typedef Elf32_Phdr  Phdr;
+    typedef Elf32_Shdr  Shdr;
+    typedef Elf32_Dyn   Dynamic;
+    typedef Elf32_Nhdr  Note;
+    typedef Elf32_Rela  Rela;
+
+    typedef Elf32_Addr  Addr;
 
 #endif
+
+#endif /* ZKTYPES_HH */
