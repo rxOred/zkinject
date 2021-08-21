3841f7b84d654f378c988f1d749283a607d5992e
rxOred <rxored@gmail.com>
2021-08-21 22:49:31 +0000

    elf

--- src/zkelf.cc
+++ src/zkelf.cc
@@ -1,14 +1,4 @@
 #include "zkelf.hh"
-#include "zkerr.hh"
-#include "zktypes.hh"
-#include <iostream>
-#include <exception>
-#include <string.h>
-#include <fcntl.h>
-#include <assert.h>
-#include <stdexcept>
-#include <sys/mman.h>
-#include <sys/stat.h>
 
 Binary::Elf::Elf()
     :elf_memmap(nullptr), elf_pathname(nullptr), elf_ehdr(nullptr),
@@ -89,14 +79,70 @@ int Binary::Elf::FindSegmentbyAttr(u32 type, u32 flags) const
         }
     }
 
-    return 0;
+    return -1;
 }
 
-
-void Binary::TextPaddingInfection::FindFreeSpace(int size) const
+int Binary::Elf::GetSectionIndexByName(const char *name) const
 {
-    bool text_found = false;
-    for(int i = 0; i < elf_ehdr->e_phnum; i++){
-        if(elf_phdr[i].p_type)
+    char *memmap = (char *)elf_memmap;
+    char *shstrtab = &memmap[elf_shdr[elf_ehdr->e_shstrndx].sh_offset];
+    for(int i = 0; i< elf_ehdr->e_shnum; i++){
+        if(strcmp(&shstrtab[elf_shdr[i].sh_name], name) == 0){
+            return i;
+        }
     }
+    return -1;
+}
+
+void *Binary::Elf::ElfRead(off_t readoff, size_t size) const
+{
+    u8 *buffer = (u8 *)calloc(size, sizeof(u8));
+    if(buffer == nullptr)
+        ERROR(std::bad_alloc());
+
+    u8 *memmap = (u8 *)elf_memmap;
+    for(int i = readoff; i < readoff + size; i++){
+        buffer[i] = memmap[i];
+    }
+
+    return buffer;
+}
+
+void Binary::Elf::ElfWrite(void *buffer, off_t writeoff, size_t size)
+ const
+{
+    u8 *memmap = (u8 *)elf_memmap;
+    u8 *_buffer = (u8 *)buffer;
+    for(int i = 0; i < writeoff + size; i++){
+        _buffer[i] = memmap[i];
+    } 
+}
+
+Binary::TextPaddingInfection::TextPaddingInfection(char *target)
+    :Elf(target), tpi_shellcode(nullptr), tpi_orgentry(0), 
+    tpi_fakeetry(0)
+{}
+
+off_t Binary::TextPaddingInfection::FindFreeSpace(int size) const
+{
+    /* text segment has permission bits set to read and exec */
+    int text_index = FindSegmentbyAttr(PT_LOAD, PF_X | PF_R);
+    assert(text_index != -1 && "text segment not found");
+
+    /* data segment has permission bits set to read and write */
+    int data_index = FindSegmentbyAttr(PT_LOAD, PF_W | PF_R);
+    assert(data_index != -1 && "data segment not found");
+
+    assert(text_index + 1 == data_index);
+    int available_space = elf_phdr[data_index].p_offset - 
+        elf_phdr[text_index].p_offset;
+    assert(available_space >= size && "available free space is less     \
+            than size");
+    return elf_phdr[text_index].p_offset + elf_phdr[text_index].
+        p_filesz;
+}
+
+Binary::TextPaddingInfection::InjectCode(void)
+{
+
 }
--- src/zkelf.hh
+++ src/zkelf.hh
@@ -1,8 +1,19 @@
 #ifndef ZKINJECT_HH
 #define ZKINJECT_HH
 
+#include "zkerr.hh"
 #include "zktypes.hh"
 #include <cstdint>
+#include <elf.h>
+#include <new>
+#include <string.h>
+#include <iostream>
+#include <exception>
+#include <fcntl.h>
+#include <assert.h>
+#include <stdexcept>
+#include <sys/mman.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 
 namespace Binary{
@@ -34,15 +45,21 @@ namespace Binary{
 
             /* commonly used stuff with infectors */
             int FindSegmentbyAttr(u32 type, u32 flags) const;
+            int GetSectionIndexByName(const char *name) const;
+            void *ElfRead(off_t readoff, size_t size) const;
+            void ElfWrite(void *buffer, off_t writeoff, size_t 
+            size) const;
     };
 
     /* text padding infection */
     class TextPaddingInfection : public Elf{
         private:
             void *tpi_shellcode;
-
+            Addr tpi_orgentry;
+            Addr tpi_fakeetry;
         public:
-            void FindFreeSpace(int size) const;
+            TextPaddingInfection(char *target);
+            off_t FindFreeSpace(int size) const;
     };
 };
 
