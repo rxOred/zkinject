ebc25b050b5b023924feb2f8e9e3848eae4171f6
rxOred <rxored@gmail.com>
2021-08-27 14:15:45 +0000

    sample shellcode for testing n shit

---
+++ examples/shellcode.asm
@@ -0,0 +1,13 @@
+section .text
+    global main
+main:
+    xor     rax, rax
+    mov     rbx, "/bin/sh"
+    push    rbx
+    mov     rdi, rsp
+    mov     rsi, rax
+    mov     rax, 59
+    syscall
+
+    mov     rax, 0x0
+    ret
