520daa6db32ff01b6202bac9e642840ff467397a
rxOred <rxored@gmail.com>
2021-08-28 22:23:54 +0000

    hooks

--- include/zkhooks.hh
+++ include/zkhooks.hh
@@ -2,6 +2,7 @@
 #define ZKHOOKS_HHH
 
 #include "zkelf.hh"
+#include "zkproc.hh"
 #include "zktypes.hh"
 #include "zkexcept.hh"
 #include <sched.h>
@@ -12,11 +13,13 @@ namespace Hooks {
         /* all basic things related to hooking */
         protected:
             int     h_symindex;
-            void    *h_origaddr;
-            void    *h_fakeaddr;
+            void    *h_orig_addr;
+            void    *h_fake_addr;
+        public:
+            Hook();
     };
 
-    class ElfGotPltHook : public Hook, Binary::Elf{
+    class ElfGotPltHook : public Hook, public Binary::Elf{
         private:
             /* dynsym index of the symbol */
             int     h_symbol_index;
@@ -26,13 +29,53 @@ namespace Hooks {
             /* rel.plt section */
             Relocation  *h_relocdyn;
             Relocation  *h_relocplt;
+            void LoadRelocations(void);
         public:
             ElfGotPltHook(const char *pathname);
-            void LoadRelocations(void);
+
+            inline void SetSymbolIndex(int index){
+                h_symbol_index = index;
+            }
+
+            inline int GetSymbolIndex(void) const
+            {
+                return h_symbol_index;
+            }
+
+            inline int GetRelocPltIndex(void) const
+            {
+                return h_relocplt_index;
+            }
+
+            inline int GetRelocDynIndex(void) const
+            {
+                return h_relocdyn_index;
+            }
+
+            inline Relocation *GetRelocDyn(void) const
+            {
+                return h_relocdyn;
+            }
+
+            inline Relocation *GetRelocPlt(void) const
+            {
+                return h_relocplt;
+            }
+
             Addr GetModuleBaseAddress(const char *module_name) const;
-            void HookFunc(const char *func_name, void *replace_addr, void *
+            void HookFunc(const char *func_name, void *fake_addr, void *
                     base_addr);
             void UnhookFuction();
+    };
+
+    class ProcGotPltHook : public Hook, public Process::Proc{
+        private:
+            ElfGotPltHook *elfhook;
+        public:
+            ProcGotPltHook(pid_t pid, const char *module_name);
+            ~ProcGotPltHook();
+            void HookFunc(const char *func_name, void *fake_addr, void *
+                    base_addr);
     };
 }
 
