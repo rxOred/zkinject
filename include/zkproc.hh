f728af76ef5869aebd21db4b94540596996a368c
rxOred <rxored@gmail.com>
2021-09-09 16:23:21 +0000

    memory map

--- include/zkproc.hh
+++ include/zkproc.hh
@@ -23,10 +23,17 @@
  * such information include, memory map, command line args
  * and more
  */
-#define MASK_ONLY_BASE_ADDR     (1 << 0)
-#define CHECK_MASK(x, y)        (x & y)
+
+#define CHECK_FLAGS(x, y) ((x) & (y))
 
 namespace Process {
+
+    enum PROCESS_INFO : short {
+        PTRACE_ATTACH_NOW       = 1 << 0,
+        PTRACE_START_NOW        = 1 << 1,
+        MEMMAP_ONLY_BASE_ADDR   = 1 << 2
+    };
+
     class MemoryMap  {
             u8 mm_flag;
             std::vector<std::shared_ptr<page_t>> mm_pageinfo;
@@ -54,10 +61,15 @@ namespace Process {
 
     class Ptrace {
         private:
-            addr_t p_baseaddr;
-            
+            u8 p_flags;
+            /* NOTE make this unique */
+            std::shared_ptr<MemoryMap> p_memmap;
+            pid_t p_pid;
+            registers_t& p_registers;
         public:
-            void *ReadProcess(addr_t address, size_t buffer_sz) const;
+            Ptrace(const char *pathname, pid_t pid, registers_t& regs, u8 flags);
+            void AttachToPorcess(void) const;
+            template<class T> T ReadProcess(addr_t address, size_t buffer_sz) const;
             void WriteProcess(void *buffer, addr_t address, size_t buffer_sz);
             registers_t ReadRegisters(void) const;
             void WriteRegisters(registers_t& registers) const;
