e7e4e49818ea3bf26acf7b977c39ba2ef64d262c
rxOred <rxored@gmail.com>
2021-11-09 22:01:48 +0000

    .

--- examples/csys.c
+++
@@ -1,9 +0,0 @@
-#define _fork()         call(__NR_fork)
-
-static int call(int sys)
-{
-    asm("                           \
-        movl    8(%esp), %eax       \
-        int     $0x80               \
-    ");
-}
