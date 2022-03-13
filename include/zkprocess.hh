93a32a8a341f9d490e8e14e92912a7b93746aa7c
rxOred <rxored@gmail.com>
2022-03-13 19:56:48 +0000

    .

--- include/zkprocess.hh
+++ include/zkprocess.hh
@@ -1,6 +1,12 @@
 #ifndef ZKPROCESS_HH
 #define ZKPROCESS_HH
 
+#include "zkptrace.hh"
+#include "zksnapshot.hh"
+#include "zkmap.hh"
+#include "zklog.hh"
+#include "zktypes.hh"
+
 namespace ZkProcess {
     class Process {
         public:
