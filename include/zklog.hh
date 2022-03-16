dc0153e868feeeb8bedec68f3e796cd0b6dfd79a
rxOred <rxored@gmail.com>
2022-03-16 13:35:42 +0000

    .

--- include/zklog.hh
+++ include/zklog.hh
@@ -3,7 +3,7 @@
 
 #include <string>
 #include <memory>
-#include <queue>
+#include <stack>
 
 #define DEFAULT_LOG_COUNT  50
 
@@ -16,9 +16,10 @@ namespace ZkLog {
 
     struct logmessage_t {
         public:
-            logmessage_t(std::string string, ZK_LOG_LEVEL level)
-                :log_string(string), log_level(level)
-            {}
+            logmessage_t(std::string string, ZK_LOG_LEVEL level);
+            logmessage_t(const logmessage_t& msg) =delete;
+            logmessage_t(logmessage_t&& msg) =default;
+
             inline std::string getLogMessage(void)
             {
                 return log_string;
@@ -35,8 +36,8 @@ namespace ZkLog {
     class Log {
         public:
             Log(const Log&) =delete;
-            static Log& Get(void) { return l_instance; }
 
+            static Log& Get(void) { return l_instance; }
             inline void SetLogCount(std::size_t count) 
             {
                 l_count = count;
@@ -44,11 +45,13 @@ namespace ZkLog {
             void ClearLog(void);
             void PushLog(std::string log_string, ZK_LOG_LEVEL level);
             std::pair<std::string, ZK_LOG_LEVEL> PopLog(void);
+            ZK_LOG_LEVEL PeekLogLevel(void); 
         private:
             Log() {}
+
             static Log l_instance; 
             std::size_t l_count = DEFAULT_LOG_COUNT;
-            std::queue<logmessage_t> l_log;
+            std::stack<logmessage_t> l_log;
     };
 };
 
--- src/zklog.cc
+++ src/zklog.cc
@@ -1,23 +1,31 @@
 #include "zklog.hh"
 
+ZkLog::logmessage_t::logmessage_t(std::string string, ZK_LOG_LEVEL level)
+    :log_string(string), log_level(level)
+{}
+
 ZkLog::Log ZkLog::Log::l_instance;
 
 void ZkLog::Log::ClearLog(void) 
 {
-    std::queue<logmessage_t> empty;
+    std::stack<logmessage_t> empty;
     std::swap(l_log, empty);
 }
 
-void ZkLog::Log::PushLog(std::string log_string, ZkLog::ZK_LOG_LEVEL level)
+void ZkLog::Log::PushLog(std::string log_string, ZK_LOG_LEVEL level)
 {
     l_log.emplace(log_string, level);
 }
 
 std::pair<std::string, ZkLog::ZK_LOG_LEVEL> ZkLog::Log::PopLog(void)
 {
-    auto pair = std::make_pair(l_log.front().getLogMessage(),
-                               l_log.front().getLoglevel());
+    auto pair = std::make_pair(l_log.top().getLogMessage(),
+                               l_log.top().getLoglevel());
     l_log.pop();
     return pair;
 }
 
+ZkLog::ZK_LOG_LEVEL ZkLog::Log::PeekLogLevel(void)
+{
+    return l_log.top().getLoglevel();
+}
